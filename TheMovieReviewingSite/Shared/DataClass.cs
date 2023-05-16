fdbe21b052dc0b1253df5c4cce9aa4b58a883722
entertainer47 <102966946+entertainer47@users.noreply.github.com>
2023-05-16 23:08:39 +0500

    Add project files.

---
+++ TheMovieReviewingSite/Shared/DataClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace TheMovieReviewingSite.Shared
+{
+    internal class DataClass:DbContext
+    {
+        public DataClass(DbContextOptions<DataClass> options) : base(options)
+        {
+
+        }
+        public DbSet<ModelClass> MovieDatas { get; set; }
+    }
+}
---
+++ TheMovieReviewingSite/Shared/ModelClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheMovieReviewingSite.Shared
+{
+    internal class ModelClass
+    {
+        public string MovieName { get; set; }
+        public float RatingonIMD { get; set; }
+        public string MovieDesc { get; set; }
+    }
+}
